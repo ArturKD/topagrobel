0b901c4ef5bfb0af3eb22e1b8a7f3a3ae4f9049d
ArturKD <arturkoshal@gmail.com>
2022-03-16 22:18:36 +0100

    feature: added html,css,js

---
+++ index.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('.menu-button')
+const activeMenu = document.querySelector('.navigation-button')
+function toggle(){
+    activeMenu.classList.toggle('active')
+}
+menu.addEventListener('click', toggle);
