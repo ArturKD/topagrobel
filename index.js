2f9acead6015dfdbb1f214e18263311e79e3efbc
ArturKD <arturkoshal@gmail.com>
2022-03-23 19:25:47 +0100

    feature: added navigation-menu

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const menu = document.querySelector('.menu-button')
-const activeMenu = document.querySelector('.navigation-button')
+const menu = document.querySelector('.menu-button');
+const activeMenuButton = document.querySelector('.navigation-button');
+const activeMenuNavigation = document.querySelector('.navigation')
 function toggle(){
-    activeMenu.classList.toggle('active')
+    activeMenuButton.classList.toggle('active')
+    activeMenuNavigation.classList.toggle('active')
 }
 menu.addEventListener('click', toggle);
 
